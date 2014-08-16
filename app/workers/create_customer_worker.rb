b17738c17480137b98307895ec4e5e3c1931522b
Steve <stevemargc@gmail.com>
2014-08-16 14:52:18 -0400

    moved create subscription prior to updating at stripe so that subscription creation not delayed by stipe. Also updated with subscription status.

--- app/workers/create_customer_worker.rb
+++ app/workers/create_customer_worker.rb
@@ -1,22 +1,24 @@
 class CreateCustomerWorker
   include Sidekiq::Worker
-  attr_accessor :user, :customer
+  attr_accessor :user, :customer, :subscription
 
   def perform(user_id, plan)
     @user = User.find(user_id)
-    create_stripe_customer(plan)
     create_subscription(plan)
+    create_stripe_customer(plan)
+    update_subscription
+  end
+  
+  def create_subscription(plan)
+    @subscription = Subscription.create(user_id: @user.id, billing_email: @user.email, plan_id: plan, subscription_status_id: 1, free_trial_expiration: (Date.current + 31))
   end
   
   def create_stripe_customer(plan)
     @customer = Stripe::Customer.create(description: @user.id, email: @user.email, plan: plan)
   end
-  
-  def create_subscription(plan)
-    Subscription.create(user_id: @user.id, billing_email: @user.email, plan_id: plan, stripe_id: @customer.id, free_trial_expiration: (Date.current + 31))
+   
+  def update_subscription
+    @subscription.update_attributes(stripe_id: @customer.id)
   end
     
 end
-
-
-
